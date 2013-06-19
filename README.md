e605c1a53a0b794e5e54eda79a2173963d493203
poplax <linjiang9999@gmail.com>
2013-06-19 18:06:14 +0800

    modified:   README.md 	modified:   setup_xcode_codeSnippets.sh

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ popLaxCodeSPMac
 favorite code for me
 
 Usage:
+
 	1. check out the Code.
 	2. cd the [codeDir]
-	2. ./setup_xcode_codeSnippets.sh
+	3. ./setup_xcode_codeSnippets.sh
 
 ###Code Path : ~/Library/Developer/Xcode/UserData/CodeSnippets/
 
--- setup_xcode_codeSnippets.sh
+++ setup_xcode_codeSnippets.sh
@@ -4,5 +4,5 @@
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
 CODE_SRC=`pwd -P`
-ln -s {CODE_SRC}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-echo "\n*** XCode Poplax Code Setup Complate!!!***\n"
+ln -s ${CODE_SRC}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+echo "*** XCode Poplax Code Setup Complate!!!***"
