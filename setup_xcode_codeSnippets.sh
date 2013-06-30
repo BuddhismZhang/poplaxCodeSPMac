4b3d48a6662e2dedf3b06c813d6590363c629bda
poplax <linjiang9999@gmail.com>
2013-06-30 15:45:26 +0800

    modified:   setup_xcode_codeSnippets.sh 	themes/

--- setup_xcode_codeSnippets.sh
+++ setup_xcode_codeSnippets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#cp -r ~/Library/Developer/Xcode/UserData/CodeSnippets/ 
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
 
 CODE_SRC=`pwd -P`
 ln -s ${CODE_SRC}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp ${CODE_SRC}/themes/Ciapre.dvtcolortheme ${CODE_SRC}/themes/'Ciapre 2.dvtcolortheme' ${CODE_SRC}/themes/'Ciapre Blue.dvtcolortheme' ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 echo "*** XCode Poplax Code Setup Complate!!!***"
