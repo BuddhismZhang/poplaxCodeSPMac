e287265d9ae203ddd43da6e7c95168e696983fac
Lin Jiang <linjiang@LintekiiMac.local>
2013-07-07 16:13:31 +0800

    new file:   UncrustifyPlugin.xcplugin.tar.gz 	modified:   setup_xcode_codeSnippets.sh

--- setup_xcode_codeSnippets.sh
+++ setup_xcode_codeSnippets.sh
@@ -1,10 +1,58 @@
 #!/bin/bash
+# Test on Xcode4.x and Xcode5-DP
 
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-mv ~/Library/Developer/Xcode/UserData/FontAndColorThemes ~/Library/Developer/Xcode/UserData/FontAndColorThemes.backup
-
+XCODE_USERDATA=~/Library/Developer/Xcode/UserData
+XCODE_PLUGIN=~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/
 CODE_SRC=`pwd -P`
-ln -s ${CODE_SRC}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-ln -s ${CODE_SRC}/themes ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+
+
+if ! [ -d $XCODE_USERDATA/CodeSnippets ]
+then
+    echo "create dir $XCODE_USERDATA/CodeSnippets"
+    mkdir -p $XCODE_USERDATA/CodeSnippets
+fi
+if ! [ -d $XCODE_USERDATA/CodeSnippets.backup ]
+then    
+    echo "make backup $XCODE_USERDATA/CodeSnippets.backup"
+    mv $XCODE_USERDATA/CodeSnippets $XCODE_USERDATA/CodeSnippets.backup    
+fi
+
+if ! [ -d $XCODE_USERDATA/FontAndColorThemes ]
+then
+    echo "create dir $XCODE_USERDATA/FontAndColorThemes"
+    mkdir $XCODE_USERDATA/FontAndColorThemes
+fi
+if ! [ -d $XCODE_USERDATA/FontAndColorThemes.backup ]
+then
+    echo "make backup $XCODE_USERDATA/FontAndColorThemes.backup"
+    mv $XCODE_USERDATA/FontAndColorThemes $XCODE_USERDATA/FontAndColorThemes.backup    
+fi
+
+if ! [ -d "$XCODE_PLUGIN" ]
+then 
+    mkdir -p "$XCODE_PLUGIN" 
+fi
+
+if ! [ -e "$XCODE_PLUGIN/UncrustifyPlugin.xcplugin" ]
+then
+    echo Install plugin
+    cp -r UncrustifyPlugin.xcplugin.tar.gz "$XCODE_PLUGIN"
+    cd "$XCODE_PLUGIN"
+    tar -zxf UncrustifyPlugin.xcplugin.tar.gz
+    cd -
+fi
+
+if ! [ -h $XCODE_USERDATA/CodeSnippets ]
+then
+    echo "Create symbolic link $XCODE_USERDATA/CodeSnippets"
+    ln -s $CODE_SRC/CodeSnippets $XCODE_USERDATA/CodeSnippets
+fi
+
+if ! [ -h $XCODE_USERDATA/FontAndColorThemes ]
+then
+    echo "Create symbolic link $XCODE_USERDATA/FontAndColorThemes"
+    ln -s $CODE_SRC/themes $XCODE_USERDATA/FontAndColorThemes
+fi
+
 #cp ${CODE_SRC}/themes/Ciapre.dvtcolortheme ${CODE_SRC}/themes/'Ciapre 2.dvtcolortheme' ${CODE_SRC}/themes/'Ciapre Blue.dvtcolortheme' ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 echo "*** XCode Poplax Code Setup Complate!!!***"
