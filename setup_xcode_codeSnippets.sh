414389abc24923e8cef478fef7934615b6178952
poplax <linjiang9999@gmail.com>
2013-07-11 14:57:49 +0800

    modified:   setup_xcode_codeSnippets.sh

--- setup_xcode_codeSnippets.sh
+++ setup_xcode_codeSnippets.sh
@@ -8,11 +8,6 @@ XCODE_PLUGIN_NAME="UncrustifyPlugin OMQuickHelp"
 XCODE_USERDATA_CONFIG_DIR="CodeSnippets FontAndColorThemes KeyBindings"
 CODE_SRC=`pwd -P`
 
-#for a in $XCODE_USERDATA_CONFIG_DIR
-#do
-#	echo "$XCODE_USERDATA/KeyBindings/$a.idekeybindings"
-#done
-#exit
 
 #Install My Config
 for user_config in $XCODE_USERDATA_CONFIG_DIR
@@ -53,52 +48,5 @@ do
 		cd -
 	fi
 done
-
-#if ! [ -e "$XCODE_PLUGIN/OMQuickHelp.xcplugin" ]
-#then
-#    echo Install plugin
-#    cp -r OMQuickHelp.xcplugin.tar.gz "$XCODE_PLUGIN"
-#    cd "$XCODE_PLUGIN"
-#    tar -zxf OMQuickHelp.xcplugin.tar.gz
-#    cd -
-#fi
-
-#if ! [ -d $XCODE_USERDATA/FontAndColorThemes ]
-#then
-#    echo "create dir $XCODE_USERDATA/FontAndColorThemes"
-#    mkdir $XCODE_USERDATA/FontAndColorThemes
-#else
-#	if ! [ -d $XCODE_USERDATA/FontAndColorThemes.backup ]
-#	then
-#		echo "make backup $XCODE_USERDATA/FontAndColorThemes.backup"
-#		mv $XCODE_USERDATA/FontAndColorThemes $XCODE_USERDATA/FontAndColorThemes.backup    
-#	fi
-#fi
-#
-#if ! [ -f $XCODE_USERDATA/KeyBindings/poplax.idekeybindings ]
-#then
-#	echo "install XCode keybindings"
-#	cp $XCODE_USERDATA/KeyBindings/poplax.idekeybindings $XCODE_USERDATA/KeyBindings/
-#fi
-
-#if ! [ -h $XCODE_USERDATA/KeyBindings ]
-#then
-#    echo "Create symbolic link $XCODE_USERDATA/KeyBindings"
-#    ln -s $CODE_SRC/KeyBindings $XCODE_USERDATA/KeyBindings
-#fi
-#
-#if ! [ -h $XCODE_USERDATA/CodeSnippets ]
-#then
-#    echo "Create symbolic link $XCODE_USERDATA/CodeSnippets"
-#    ln -s $CODE_SRC/CodeSnippets $XCODE_USERDATA/CodeSnippets
-#fi
-#
-#if ! [ -h $XCODE_USERDATA/FontAndColorThemes ]
-#then
-#    echo "Create symbolic link $XCODE_USERDATA/FontAndColorThemes"
-#    ln -s $CODE_SRC/themes $XCODE_USERDATA/FontAndColorThemes
-#fi
-
-#cp ${CODE_SRC}/themes/Ciapre.dvtcolortheme ${CODE_SRC}/themes/'Ciapre 2.dvtcolortheme' ${CODE_SRC}/themes/'Ciapre Blue.dvtcolortheme' ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
 echo "*** XCode Poplax Code Setup Complate!!!***"
